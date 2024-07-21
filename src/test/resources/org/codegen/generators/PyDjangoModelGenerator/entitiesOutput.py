314d27a1a38e587befe95ddf8fb2e5627972e3e0
atten <art@force.fm>
2024-07-21 23:24:43 +0300

    fix prev

--- src/test/resources/org/codegen/generators/PyDjangoModelGenerator/entitiesOutput.py
+++ src/test/resources/org/codegen/generators/PyDjangoModelGenerator/entitiesOutput.py
@@ -13,7 +13,7 @@ class EnumValue(models.TextChoices):
 
 class BasicDTO(models.Model):
     timestamp = models.DateTimeField()
-    duration = models.CharField()
+    duration = models.CharField(max_length=32)
     optional_value = models.FloatField(default=0)
     nullable_value = models.BooleanField(blank=True, null=True)
     enum_value = models.CharField(choices=EnumValue.choices, max_length=7)
