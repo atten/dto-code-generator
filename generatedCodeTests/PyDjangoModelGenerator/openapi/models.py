c0527bcf2e04aef5c6c452b79b1136eb0709c755
atten <art@force.fm>
2025-01-02 12:28:35 +0300

    add generated code tests for django openapi output

---
+++ generatedCodeTests/PyDjangoModelGenerator/openapi/models.py
@@ -0,0 +1,9 @@
+from generated.openapi_models import *
+
+
+class BasicModel(BasicDto):
+    pass
+
+
+class AdvancedModel(AdvancedDto):
+    pass
--- generatedCodeTests/PyDjangoModelGenerator/run.sh
+++ generatedCodeTests/PyDjangoModelGenerator/run.sh
@@ -1,4 +1,5 @@
 mkdir -p generated/
 cp ../../src/test/resources/org/codegen/generators/PyDjangoModelGenerator/entitiesOutput.py generated/entities_models.py
-rm -rf entities/migrations
+cp ../../src/test/resources/org/codegen/generators/PyDjangoModelGenerator/openApiOutput.py generated/openapi_models.py
+rm -rf entities/migrations openapi/migrations
 docker compose --progress quiet up --build --abort-on-container-exit
